af0dff5ec82c38b7d8eef445f28e454bf62f6152
vuong-GitHud <71747063+vuong-GitHud@users.noreply.github.com>
2020-11-05 18:33:02 +0700

    Update 31.01-server.js

--- 31.01-server.js
+++ 31.01-server.js
@@ -1,17 +1,25 @@
-http = require("http");
-
 const hostname = '127.0.0.1';
-const port = 8080;
+const port = process.env.PORT || 8080;
 
-server = http.createServer(
-    (req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.write('chua co gi !');
-        res.end();
-    }
-);
 
-server.listen( port, hostname, 
-    () => { console.log(' Server running: ',hostname, process.env.PORT || 8080 ); }
-);
+/// ------------------ Khai bao LIB de su dung
+var express = require('express');
+var session = require('express-session');
+var app = express();
+
+/// ..................................................
+app.get('/', homePage);
+function homePage(req, res) {
+    res.send("HOME !");   
+    console.log("\n\t ... connect from ", req.connection.remoteAddress, req.headers.host);
+}
+
+/// ------------------ gọi SERVER thực thi
+
+
+var server = app.listen( port , function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("SERVER http://%s:%s", host, port)
+});
